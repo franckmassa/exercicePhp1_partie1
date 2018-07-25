f78dec26375a376568285121c05ce5b0eb8a6fd6
massa <massafranck@gmail.com>
2018-07-25 19:55:39 +0200

    initial

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$name = 'Franck';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice1</title>
+</head>
+<body>
+  <p>
+    <?php
+    echo $name;
+    ?>
+  </p>
+</body>
+</html>
